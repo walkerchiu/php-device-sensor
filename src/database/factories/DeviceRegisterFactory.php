0c7856d416eda530640047233860739d1881f573
walkerchiu <chenjen.chiou@gmail.com>
2022-02-07 08:37:40 +0800

    Initial commit

---
+++ src/database/factories/DeviceRegisterFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory  $factory */
+
+use Faker\Generator as Faker;
+use WalkerChiu\Core\Models\Constants\DataType;
+use WalkerChiu\DeviceSensor\Models\Entities\DeviceRegister;
+use WalkerChiu\DeviceSensor\Models\Entities\DeviceRegisterLang;
+
+$factory->define(DeviceRegister::class, function (Faker $faker) {
+    return [
+        'serial'     => $faker->isbn10,
+        'identifier' => $faker->slug,
+        'mean'       => $faker->slug,
+        'data_type'  => $faker->randomElement(DataType::getCodes())
+    ];
+});
+
+$factory->define(DeviceRegisterLang::class, function (Faker $faker) {
+    return [
+        'code'  => $faker->locale,
+        'key'   => $faker->randomElement(['name', 'description']),
+        'value' => $faker->sentence
+    ];
+});
---
+++ src/database/factories/DeviceStateFactory.php
@@ -0,0 +1,23 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory  $factory */
+
+use Faker\Generator as Faker;
+use WalkerChiu\DeviceSensor\Models\Entities\DeviceState;
+use WalkerChiu\DeviceSensor\Models\Entities\DeviceStateLang;
+
+$factory->define(DeviceState::class, function (Faker $faker) {
+    return [
+        'serial'     => $faker->isbn10,
+        'identifier' => $faker->slug,
+        'mean'       => $faker->slug
+    ];
+});
+
+$factory->define(DeviceStateLang::class, function (Faker $faker) {
+    return [
+        'code'  => $faker->locale,
+        'key'   => $faker->randomElement(['name', 'description']),
+        'value' => $faker->sentence
+    ];
+});
