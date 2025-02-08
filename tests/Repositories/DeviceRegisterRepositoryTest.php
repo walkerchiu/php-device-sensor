f5911a14edab8e2eae94f9790c4def6bc4b48ed6
walkerchiu <chenjen.chiou@gmail.com>
2025-02-08 14:17:40 +0800

    Test(Project): Introduce unit tests using orchestra/testbench

---
+++ tests/Repositories/DeviceRegisterRepositoryTest.php
@@ -0,0 +1,421 @@
+<?php
+
+namespace WalkerChiu\DeviceSensor;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Support\Facades\Config;
+use WalkerChiu\Core\Models\Constants\DataType;
+use WalkerChiu\DeviceSensor\Models\Entities\Device;
+use WalkerChiu\DeviceSensor\Models\Entities\DeviceRegister;
+use WalkerChiu\DeviceSensor\Models\Entities\DeviceRegisterLang;
+use WalkerChiu\DeviceSensor\Models\Repositories\DeviceRegisterRepository;
+
+class DeviceRegisterRepositoryTest extends \Orchestra\Testbench\TestCase
+{
+    use RefreshDatabase;
+
+    protected $repository;
+
+    /**
+     * Setup the test environment.
+     *
+     * @return void
+     */
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->loadMigrationsFrom(__DIR__ .'/../migrations');
+        $this->withFactories(__DIR__ .'/../../src/database/factories');
+
+        $this->repository = $this->app->make(DeviceRegisterRepository::class);
+    }
+
+    /**
+     * To load your package service provider, override the getPackageProviders.
+     *
+     * @param \Illuminate\Foundation\Application  $app
+     * @return Array
+     */
+    protected function getPackageProviders($app)
+    {
+        return [\WalkerChiu\Core\CoreServiceProvider::class,
+                \WalkerChiu\DeviceSensor\DeviceSensorServiceProvider::class];
+    }
+
+    /**
+     * Define environment setup.
+     *
+     * @param \Illuminate\Foundation\Application  $app
+     * @return void
+     */
+    protected function getEnvironmentSetUp($app)
+    {
+    }
+
+    /**
+     * A basic functional test on DeviceRegisterRepository.
+     *
+     * For WalkerChiu\Core\Models\Repositories\Repository
+     *
+     * @return void
+     */
+    public function testDeviceRegisterRepository()
+    {
+        // Config
+        Config::set('wk-core.onoff.core-lang_core', 0);
+        Config::set('wk-device-sensor.onoff.core-lang_core', 0);
+        Config::set('wk-core.lang_log', 1);
+        Config::set('wk-device-sensor.lang_log', 1);
+        Config::set('wk-core.soft_delete', 1);
+        Config::set('wk-device-sensor.soft_delete', 1);
+
+        // Give
+        $faker = \Faker\Factory::create();
+        $db_device = factory(Device::class)->create();
+        $id_list = [];
+        for ($i=1; $i<=3; $i++) {
+            $record = $this->repository->save([
+                'device_id'  => $db_device->id,
+                'serial'     => $faker->isbn10,
+                'identifier' => $faker->slug,
+                'mean'       => $faker->slug,
+                'data_type'  => $faker->randomElement(DataType::getCodes())
+            ]);
+            array_push($id_list, $record->id);
+        }
+
+        // Get and Count records after creation
+            // When
+            $records = $this->repository->get();
+            $count   = $this->repository->count();
+            // Then
+            $this->assertCount(3, $records);
+            $this->assertEquals(3, $count);
+
+        // Find someone
+            // When
+            $record = $this->repository->first();
+            // Then
+            $this->assertNotNull($record);
+
+            // When
+            $record = $this->repository->find($faker->uuid());
+            // Then
+            $this->assertNull($record);
+
+        // Delete someone
+            // When
+            $this->repository->deleteByIds([$id_list[0]]);
+            $count = $this->repository->count();
+            // Then
+            $this->assertEquals(2, $count);
+
+            // When
+            $this->repository->deleteByExceptIds([$id_list[2]]);
+            $count = $this->repository->count();
+            $record = $this->repository->find($id_list[2]);
+            // Then
+            $this->assertEquals(1, $count);
+            $this->assertNotNull($record);
+
+            // When
+            $count = $this->repository->where('id', '>', 0)->count();
+            // Then
+            $this->assertEquals(1, $count);
+
+            // When
+            $count = $this->repository->whereWithTrashed('id', '>', 0)->count();
+            // Then
+            $this->assertEquals(3, $count);
+
+            // When
+            $count = $this->repository->whereOnlyTrashed('id', '>', 0)->count();
+            // Then
+            $this->assertEquals(2, $count);
+
+        // Force delete someone
+            // When
+            $this->repository->forcedeleteByIds([$id_list[2]]);
+            $records = $this->repository->get();
+            // Then
+            $this->assertCount(0, $records);
+
+        // Restore records
+            // When
+            $this->repository->restoreByIds([$id_list[0], $id_list[1]]);
+            $count = $this->repository->count();
+            // Then
+            $this->assertEquals(2, $count);
+    }
+
+    /**
+     * Unit test about Lang creation on DeviceRegisterRepository.
+     *
+     * For WalkerChiu\Core\Models\Repositories\RepositoryTrait
+     *     WalkerChiu\DeviceSensor\Models\Repositories\DeviceRegisterRepository
+     * 
+     * @return void
+     */
+    public function testcreateLangWithoutCheck()
+    {
+        // Config
+        Config::set('wk-core.onoff.core-lang_core', 0);
+        Config::set('wk-device-sensor.onoff.core-lang_core', 0);
+        Config::set('wk-core.lang_log', 1);
+        Config::set('wk-device-sensor.lang_log', 1);
+        Config::set('wk-core.soft_delete', 1);
+        Config::set('wk-device-sensor.soft_delete', 1);
+
+        // Give
+        $db_device = factory(Device::class)->create();
+        $db_register = factory(DeviceRegister::class)->create(['device_id' => $db_device->id]);
+
+        // Find record
+            // When
+            $record = $this->repository->first();
+            // Then
+            $this->assertNotNull($record);
+
+        // Create Lang
+            // When
+            $lang = $this->repository->createLangWithoutCheck(['morph_type' => get_class($record), 'morph_id' => $record->id, 'code' => 'en_us', 'key' => 'name', 'value' => 'Hello']);
+            // Then
+            $this->assertInstanceOf(DeviceRegisterLang::class, $lang);
+    }
+
+    /**
+     * Unit test about Enable and Disable on DeviceRegisterRepository.
+     *
+     * For WalkerChiu\Core\Models\Repositories\RepositoryTrait
+     *     WalkerChiu\DeviceSensor\Models\Repositories\DeviceRegisterRepository
+     *
+     * @return void
+     */
+    public function testEnableAndDisable()
+    {
+        // Config
+        Config::set('wk-core.onoff.core-lang_core', 0);
+        Config::set('wk-device-sensor.onoff.core-lang_core', 0);
+        Config::set('wk-core.lang_log', 1);
+        Config::set('wk-device-sensor.lang_log', 1);
+        Config::set('wk-core.soft_delete', 1);
+        Config::set('wk-device-sensor.soft_delete', 1);
+
+        // Give
+        $db_device = factory(Device::class)->create();
+        $db_morph_1 = factory(DeviceRegister::class)->create(['device_id' => $db_device->id, 'is_enabled' => 1]);
+        $db_morph_2 = factory(DeviceRegister::class)->create(['device_id' => $db_device->id]);
+        $db_morph_3 = factory(DeviceRegister::class)->create(['device_id' => $db_device->id]);
+        $db_morph_4 = factory(DeviceRegister::class)->create(['device_id' => $db_device->id]);
+
+        // Count records
+            // When
+            $count = $this->repository->count();
+            $count_enabled = $this->repository->ofEnabled()->count();
+            $count_disabled = $this->repository->ofDisabled()->count();
+            // Then
+            $this->assertEquals(4, $count);
+            $this->assertEquals(1, $count_enabled);
+            $this->assertEquals(3, $count_disabled);
+
+        // Enable records
+            // When
+            $this->repository->whereToEnable('id', '=', $db_morph_4->id);
+            $count_enabled = $this->repository->ofEnabled()->count();
+            $count_disabled = $this->repository->ofDisabled()->count();
+            // Then
+            $this->assertEquals(2, $count_enabled);
+            $this->assertEquals(2, $count_disabled);
+
+        // Disable records
+            // When
+            $this->repository->whereToDisable('id', '>', 0);
+            $count_enabled = $this->repository->ofEnabled()->count();
+            $count_disabled = $this->repository->ofDisabled()->count();
+            // Then
+            $this->assertEquals(0, $count_enabled);
+            $this->assertEquals(4, $count_disabled);
+    }
+
+    /**
+     * Unit test about Query List on DeviceRegisterRepository.
+     *
+     * For WalkerChiu\Core\Models\Repositories\RepositoryTrait
+     *     WalkerChiu\DeviceSensor\Models\Repositories\DeviceRegisterRepository
+     *
+     * @return void
+     */
+    public function testQueryList()
+    {
+        // Config
+        Config::set('wk-core.onoff.core-lang_core', 0);
+        Config::set('wk-device-sensor.onoff.core-lang_core', 0);
+        Config::set('wk-core.lang_log', 1);
+        Config::set('wk-device-sensor.lang_log', 1);
+        Config::set('wk-core.soft_delete', 1);
+        Config::set('wk-device-sensor.soft_delete', 1);
+
+        $faker = \Faker\Factory::create();
+
+        // Give
+        $db_device = factory(Device::class)->create();
+        $db_morph_1 = factory(DeviceRegister::class)->create(['device_id' => $db_device->id]);
+        $db_morph_2 = factory(DeviceRegister::class)->create(['device_id' => $db_device->id]);
+        $db_morph_3 = factory(DeviceRegister::class)->create(['device_id' => $db_device->id]);
+        $db_morph_4 = factory(DeviceRegister::class)->create(['device_id' => $db_device->id]);
+
+        // Get query
+            // When
+            sleep(1);
+            $this->repository->find($db_morph_3->id)->touch();
+            $records = $this->repository->ofNormal()->get();
+            // Then
+            $this->assertCount(4, $records);
+
+            // When
+            $record = $records->first();
+            // Then
+            $this->assertArrayNotHasKey('deleted_at', $record->toArray());
+            $this->assertEquals($db_morph_3->id, $record->id);
+
+        // Get query of trashed records
+            // When
+            $this->repository->deleteByIds([$db_morph_4->id]);
+            $this->repository->deleteByIds([$db_morph_1->id]);
+            $records = $this->repository->ofTrash()->get();
+            // Then
+            $this->assertCount(2, $records);
+
+            // When
+            $record = $records->first();
+            // Then
+            $this->assertArrayHasKey('deleted_at', $record);
+            $this->assertEquals($db_morph_1->id, $record->id);
+    }
+
+    /**
+     * Unit test about FormTrait on DeviceRegisterRepository.
+     *
+     * For WalkerChiu\Core\Models\Repositories\RepositoryTrait
+     *     WalkerChiu\DeviceSensor\Models\Repositories\DeviceRegisterRepository
+     *     WalkerChiu\Core\Models\Forms\FormTrait
+     *
+     * @return void
+     */
+    public function testFormTrait()
+    {
+        // Config
+        Config::set('wk-core.onoff.core-lang_core', 0);
+        Config::set('wk-device-sensor.onoff.core-lang_core', 0);
+        Config::set('wk-core.lang_log', 1);
+        Config::set('wk-device-sensor.lang_log', 1);
+        Config::set('wk-core.soft_delete', 1);
+        Config::set('wk-device-sensor.soft_delete', 1);
+
+        $faker = \Faker\Factory::create();
+
+        // Name
+            // Give
+            $db_device = factory(Device::class)->create();
+            $db_morph_1 = factory(DeviceRegister::class)->create(['device_id' => $db_device->id]);
+            $db_morph_2 = factory(DeviceRegister::class)->create(['device_id' => $db_device->id]);
+            $db_lang_1 = $this->repository->createLangWithoutCheck(['morph_id' => $db_morph_1->id, 'morph_type' => DeviceRegister::class, 'code' => 'en_us', 'key' => 'name', 'value' => 'Hello']);
+            $db_lang_2 = $this->repository->createLangWithoutCheck(['morph_id' => $db_morph_2->id, 'morph_type' => DeviceRegister::class, 'code' => 'zh_tw', 'key' => 'name', 'value' => '您好']);
+            // When
+            $result_1 = $this->repository->checkExistName('en_us', null, 'Hello');
+            $result_2 = $this->repository->checkExistName('en_us', null, 'Hi');
+            $result_3 = $this->repository->checkExistName('en_us', $db_morph_1->id, 'Hello');
+            $result_4 = $this->repository->checkExistName('en_us', $db_morph_1->id, '您好');
+            $result_5 = $this->repository->checkExistName('zh_tw', $db_morph_1->id, '您好');
+            $result_6 = $this->repository->checkExistNameOfEnabled('en_us', null, 'Hello');
+            // Then
+            $this->assertTrue($result_1);
+            $this->assertTrue(!$result_2);
+            $this->assertTrue(!$result_3);
+            $this->assertTrue(!$result_4);
+            $this->assertTrue($result_5);
+            $this->assertTrue(!$result_6);
+
+        // Serial, Identifier
+            // Give
+            $db_morph_3 = factory(DeviceRegister::class)->create(['device_id' => $db_device->id, 'serial' => '123', 'identifier' => 'A123']);
+            $db_morph_4 = factory(DeviceRegister::class)->create(['device_id' => $db_device->id, 'serial' => '124', 'identifier' => 'A124']);
+            $db_morph_5 = factory(DeviceRegister::class)->create(['device_id' => $db_device->id, 'serial' => '125', 'identifier' => 'A125', 'is_enabled' => 0]);
+
+            // When
+            $result_1 = $this->repository->checkExistSerial(null, '123');
+            $result_2 = $this->repository->checkExistSerial($db_morph_3->id, '123');
+            $result_3 = $this->repository->checkExistSerial($db_morph_3->id, '124');
+            $result_4 = $this->repository->checkExistSerialOfEnabled($db_morph_4->id, '124');
+            $result_5 = $this->repository->checkExistSerialOfEnabled($db_morph_4->id, '125');
+            // Then
+            $this->assertTrue($result_1);
+            $this->assertTrue(!$result_2);
+            $this->assertTrue($result_3);
+            $this->assertTrue(!$result_4);
+            $this->assertTrue(!$result_5);
+
+            // When
+            $result_1 = $this->repository->checkExistIdentifier(null, 'A123');
+            $result_2 = $this->repository->checkExistIdentifier($db_morph_3->id, 'A123');
+            $result_3 = $this->repository->checkExistIdentifier($db_morph_3->id, 'A124');
+            $result_4 = $this->repository->checkExistIdentifierOfEnabled($db_morph_4->id, 'A124');
+            $result_5 = $this->repository->checkExistIdentifierOfEnabled($db_morph_4->id, 'A125');
+            // Then
+            $this->assertTrue($result_1);
+            $this->assertTrue(!$result_2);
+            $this->assertTrue($result_3);
+            $this->assertTrue(!$result_4);
+            $this->assertTrue(!$result_5);
+    }
+
+    /**
+     * Unit test about Auto Complete on DeviceRegisterRepository.
+     *
+     * For WalkerChiu\Core\Models\Repositories\RepositoryTrait
+     *     WalkerChiu\DeviceSensor\Models\Repositories\DeviceRegisterRepository
+     *
+     * @return void
+     */
+    public function testAutoComplete()
+    {
+        // Config
+        Config::set('wk-core.onoff.core-lang_core', 0);
+        Config::set('wk-device-sensor.onoff.core-lang_core', 0);
+        Config::set('wk-core.lang_log', 1);
+        Config::set('wk-device-sensor.lang_log', 1);
+        Config::set('wk-core.soft_delete', 1);
+        Config::set('wk-device-sensor.soft_delete', 1);
+
+        $faker = \Faker\Factory::create();
+
+        // Give
+        $db_device = factory(Device::class)->create();
+        $db_morph_1 = factory(DeviceRegister::class)->create(['device_id' => $db_device->id, 'serial' => 'A123', 'is_enabled' => 1]);
+        $db_morph_2 = factory(DeviceRegister::class)->create(['device_id' => $db_device->id, 'serial' => 'A124', 'is_enabled' => 1]);
+        $db_lang_1 = $this->repository->createLangWithoutCheck(['morph_id' => $db_morph_1->id, 'morph_type' => DeviceRegister::class, 'code' => 'en_us', 'key' => 'name', 'value' => 'Hello']);
+        $db_lang_2 = $this->repository->createLangWithoutCheck(['morph_id' => $db_morph_1->id, 'morph_type' => DeviceRegister::class, 'code' => 'en_us', 'key' => 'description', 'value' => 'Good Morning!']);
+        $db_lang_3 = $this->repository->createLangWithoutCheck(['morph_id' => $db_morph_1->id, 'morph_type' => DeviceRegister::class, 'code' => 'en_us', 'key' => 'name', 'value' => 'Hello World']);
+        $db_lang_4 = $this->repository->createLangWithoutCheck(['morph_id' => $db_morph_1->id, 'morph_type' => DeviceRegister::class, 'code' => 'zh_tw', 'key' => 'name', 'value' => '您好']);
+        $db_lang_5 = $this->repository->createLangWithoutCheck(['morph_id' => $db_morph_1->id, 'morph_type' => DeviceRegister::class, 'code' => 'zh_tw', 'key' => 'name', 'value' => '早安']);
+        $db_lang_6 = $this->repository->createLangWithoutCheck(['morph_id' => $db_morph_2->id, 'morph_type' => DeviceRegister::class, 'code' => 'en_us', 'key' => 'name', 'value' => 'Bye']);
+
+        // List array by name of enabled records
+            // When
+            $records = $this->repository->autoCompleteNameOfEnabled('en_us', 'H');
+            // Then
+            $this->assertCount(1, $records);
+
+            // When
+            $records = $this->repository->autoCompleteNameOfEnabled('zh_tw', 'H');
+            // Then
+            $this->assertCount(0, $records);
+
+        // List array by serial of enabled records
+            // When
+            $records = $this->repository->autoCompleteSerialOfEnabled('en_us', 'A');
+            // Then
+            $this->assertCount(2, $records);
+    }
+}
